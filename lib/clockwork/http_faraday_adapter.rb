34d9ef69ef6df511a68274bbc1644f820f894056
Kajetan Bojko <kai@shutl.co.uk>
2013-03-08 11:49:40 +0000

    wip

--- lib/clockwork/http_faraday_adapter.rb
+++ lib/clockwork/http_faraday_adapter.rb
@@ -1,14 +1,19 @@
 module Clockwork
   class HttpFaradayAdapter
     class << self
+      attr_accessor :adapter
+
       def connection
         Faraday.new(:url => 'http://sushi.com') do |faraday|
-          faraday.request  :url_encoded             # form-encode POST params
+          #faraday.request  :url_encoded             # form-encode POST params
           faraday.response :logger                  # log requests to STDOUT
-          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+          faraday.adapter  adapter  # make requests with Net::HTTP
         end
       end
 
+      def adapter
+Faraday.default_adapter
+      end
       def post url, data = '', use_ssl = true
         uri = if use_ssl
                 URI.parse "https://#{url}"
