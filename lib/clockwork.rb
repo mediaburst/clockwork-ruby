3360522712d4ca2a5e87bd959175f5d187f0065c
Martin Steel <martin@mediaburst.co.uk>
2014-06-30 14:47:47 +0100

    Ruby 1.8 support
    Requested by end users, tested under Ruby 1.8.7

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -2,6 +2,7 @@ lib_dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 
 require 'net/http'
+require 'net/https'
 require 'nokogiri'
 
 require 'clockwork/error'
